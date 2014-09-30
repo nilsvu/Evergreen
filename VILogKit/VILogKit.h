3c7d55bc1e156a0cba9cc2d18c2ab3d4f648eb88
Nils Fischer <n.fischer@viwid.com>
2014-09-30 11:46:25 -0600

    Initial Commit

---
+++ VILogKit/VILogKit.h
@@ -0,0 +1,19 @@
+//
+//  VILogKit.h
+//  VILogKit
+//
+//  Created by Nils Fischer on 30.09.14.
+//  Copyright (c) 2014 viWiD Webdesign & iOS Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VILogKit.
+FOUNDATION_EXPORT double VILogKitVersionNumber;
+
+//! Project version string for VILogKit.
+FOUNDATION_EXPORT const unsigned char VILogKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VILogKit/PublicHeader.h>
+
+
